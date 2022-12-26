d95dde1488f52af238c3755dd2c8723514c6e54c
Vitalick37 <vit198837@mail.ru>
2022-12-26 19:36:13 +0300

    follow/unfollow with cookie

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,80 +3,112 @@ import classes from "./Users.module.css";
 import cn from "classnames";
 import userNoName from "../../img/userNoName.jpg";
 import { NavLink } from "react-router-dom";
+import axios from "axios";
 
 const Users = (props) => {
+  let pagesCount = Math.ceil(props.count / props.pageSize);
 
-    let pagesCount = Math.ceil(props.count / props.pageSize);
+  let pages = [];
 
-    let pages = [];
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i);
+  }
 
-    for (let i = 1; i <= pagesCount; i++) {
-        pages.push(i);
-    }
-
-    return (
-        
+  return (
     <div className={classes.users}>
-        <ul className={classes.pages}>
+      <ul className={classes.pages}>
         {pages.map((p) => {
-            return (
+          return (
             <li
-                className={cn(classes.page, props.currentPage === p && classes.pageActive)}
-                onClick={(e) => {props.onPageChanged(p);}}
+              className={cn(
+                classes.page,
+                props.currentPage === p && classes.pageActive
+              )}
+              onClick={(e) => {
+                props.onPageChanged(p);
+              }}
             >
-                {p}
+              {p}
             </li>
-            );
+          );
         })}
-        </ul>
+      </ul>
 
-        {props.users.map((e) => (
+      {props.users.map((e) => (
         <div className={classes.users_container} key={e.id}>
-            <div className={classes.users_right}>
+          <div className={classes.users_right}>
             <div className={classes.users_img}>
-                <NavLink to={'/profile' + '/' + e.id}>
+              <NavLink to={"/profile" + "/" + e.id}>
                 <img
-                src={e.photos.small != null ? e.photos.small : userNoName}
-                alt="photo"
+                  src={e.photos.small != null ? e.photos.small : userNoName}
+                  alt="photo"
                 />
-                </NavLink>
+              </NavLink>
             </div>
             <div>
-                {e.followed ? (
+              {e.followed ? (
                 <button
-                    onClick={() => {
-                    props.unFollow(e.id);
-                    }}
-                >
-                    Unfollow
-                </button>
-                ) : (
+                  onClick={() => {
+
+                    axios
+                      .delete(
+                        `https://social-network.samuraijs.com/api/1.0/follow/${e.id}`, { 
+                            withCredentials: true, 
+                            headers: {
+                                "API-KEY": 'd3e9db55-8986-4705-a8d3-b42af80b5ce1',
+                            },
+                        }
+                      )
+                      .then((response) => {
+                        // debugger;
+                        if (response.data.resultCode === 0) {
+                          props.unFollow(e.id);
+                        }
+                      });
+                  }}
+
+                >Unfollow</button>
+              ) : (
                 <button
-                    onClick={() => {
-                    props.follow(e.id);
-                    }}
-                >
-                    Follow
-                </button>
-                )}
+                  onClick={() => {
+
+                    axios
+                      .post(
+                        `https://social-network.samuraijs.com/api/1.0/follow/${e.id}`, null, { 
+                            withCredentials: true,
+                            headers: {
+                                "API-KEY": 'd3e9db55-8986-4705-a8d3-b42af80b5ce1',
+                            },
+                        }
+                      )
+                      .then((response) => {
+                        // debugger;
+                        if (response.data.resultCode === 0) {
+                          props.follow(e.id);
+                        }
+                      });
+                  }}
+
+                >Follow</button>
+              )}
             </div>
-            </div>
-            <div className={classes.users_left}>
+          </div>
+          <div className={classes.users_left}>
             <div>
-                <div className={classes.users_name}>{e.name}</div>
-                <div className={classes.users_status}>{e.status}</div>
+              <div className={classes.users_name}>{e.name}</div>
+              <div className={classes.users_status}>{e.status}</div>
             </div>
             <div>
-                <div className={classes.users_city}>{"e.location.city"}</div>
-                <div className={classes.users_country}>
+              <div className={classes.users_city}>{"e.location.city"}</div>
+              <div className={classes.users_country}>
                 {"e.location.country"}
-                </div>
+              </div>
             </div>
-            </div>
+          </div>
         </div>
-        ))}
+      ))}
     </div>
-    );
+  );
 };
 
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -14,8 +14,7 @@ class UsersApiComponent extends React.Component {
     componentDidMount() {
       this.props.toggleIsFetching(true);
       axios.get(
-          `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
-        )
+          `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {withCredentials: true,})
         .then((response) => {
           this.props.toggleIsFetching(false);
           this.props.setUsers(response.data.items);
@@ -27,8 +26,7 @@ class UsersApiComponent extends React.Component {
       this.props.setCurrentPage(pageNumber);
       this.props.toggleIsFetching(true);
       axios.get(
-          `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`
-        )
+          `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, {withCredentials: true,})
         .then((response) => {
           this.props.toggleIsFetching(false);
           this.props.setUsers(response.data.items);
