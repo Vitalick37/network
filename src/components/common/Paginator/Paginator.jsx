1cbc57bb32798b9028a9d0038bfc1df39a43aaab
Vitalick37 <vit198837@mail.ru>
2023-02-10 15:46:57 +0300

    pagination of count pages

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "../../Users/Users.module.css";
 import cn from "classnames";
 import userNoName from "../../../img/userNoName.jpg";
@@ -17,10 +17,22 @@ const Paginator = (props) => {
     pages.push(i);
   }
 
+  let portionSize = 10;
+
+  let portionCount = Math.ceil(pagesCount / portionSize);
+  let [portionNumber, setPortionNumbet] = useState(1);
+  let leftPortionNumber = (portionNumber - 1) * portionSize + 1;
+  let rightPortionNumber = portionNumber * portionSize;
+  console.log(portionNumber);
+
   return (
+  <div className={classes.paginator}>
+      {portionNumber > 1 && <button onClick={() => setPortionNumbet(portionNumber - 1)}>PREV</button>}
 
       <ul className={classes.pages}>
-        {pages.map((p) => {
+        {pages
+        .filter(p => p >= leftPortionNumber && p <= rightPortionNumber)
+        .map((p) => {
           return (
             <li
               className={cn(classes.page, props.currentPage === p && classes.pageActive)}
@@ -30,6 +42,8 @@ const Paginator = (props) => {
         })}
       </ul>
 
+      {portionCount > portionNumber && <button onClick={() => setPortionNumbet(portionNumber + 1)}>NEXT</button>}
+  </div>
   );
 };
 
